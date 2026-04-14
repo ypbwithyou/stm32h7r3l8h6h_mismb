ea6efb3caca6587ef62ae10bcfd261ffdc8072b3
ypb <ypbwithyou@hotmail.com>
2026-04-14 11:17:10 +0800

    1，桥路协议确认

--- User/rs485_processor.h
+++ User/rs485_processor.h
@@ -12,7 +12,7 @@ enum Event
 {
     DEVINFO_READ_REQ = 0x00,
     DEVINFO_READ_REQ_ACK = 0x01,
-    
+
     DEVINFO_WRITE_REQ = 0x02,
     DEVINFO_WRITE_REQ_ACK = 0x03,
 
@@ -38,25 +38,25 @@ typedef struct
 /* 校准设置结构体 (DAC_SET) */
 typedef struct
 {
-    uint8_t bitflag;    // 0-2bit对应通道1-3, bit=1表示设置该通道; 全0=禁用DAC, 全1=统一设置
-    float voltage0;     // 通道0电压值
-    float voltage1;     // 通道1电压值
-    float voltage2;     // 通道2电压值
+    uint8_t bitflag; // 0-2bit对应通道1-3, bit=1表示设置该通道; 全0=禁用DAC, 全1=统一设置
+    float voltage0;  // 通道0电压值
+    float voltage1;  // 通道1电压值
+    float voltage2;  // 通道2电压值
 } __attribute__((packed)) dac_set_payload_t;
 
 /* 桥路设置结构体 (BRIDGE_SET) */
 typedef struct
 {
     uint8_t exc_en;      // 激励使能: 0=失能, 1=使能
-    uint8_t bridge;      // bit0-2: 通道0-2的桥路类型, 0=全桥, 1=半桥
+    uint8_t bridge;      // bit0-2: 通道0-2的桥路类型, 0=全桥, 1=半桥，1=1/4桥
     uint8_t bridgeShunt; // bit0-2: 通道0-2的分流电阻接入, 0=不接, 1=接入
 } __attribute__((packed)) bridge_set_payload_t;
 
 /* 增益设置结构体 (GAIN_SET) */
 typedef struct
 {
-    uint8_t gain;        // bit0-2分别表示三通道, 0=1倍, 1=10倍
-    uint16_t pga;        // bit0-2=PGA0, bit3-5=PGA1, bit6-8=PGA2; 全0=1倍, 全1=128倍
+    uint8_t gain; // bit0-2分别表示三通道, 0=1倍, 1=10倍
+    uint16_t pga; // bit0-2=PGA0, bit3-5=PGA1, bit6-8=PGA2; 全0=1倍, 全1=128倍  1 2 4 8 16 32 64 128 
 } __attribute__((packed)) gain_set_payload_t;
 
 /* Protocol layer */
